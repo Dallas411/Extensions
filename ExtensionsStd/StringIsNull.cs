f27d46c61d2278226219b9ccec56e2d2517c9fbe
Marco Dalla Libera <marco@dallalibera.net>
2018-06-15 16:05:27 +0200

    StringIsNull

---
+++ ExtensionsStd/StringIsNull.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionsStd
+{
+    public static partial class CommonExtensions
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool NotIsNullOrEmpty(this string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        public static bool NotIsNullOrWhiteSpace(this string value)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
