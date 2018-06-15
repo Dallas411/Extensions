03e15263c8c267a7eb01a294b4ddfa022b3fea8e
Marco Dalla Libera <marco@dallalibera.net>
2018-06-15 15:40:19 +0200

    Format

---
+++ ExtensionsStd/StringFormat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionsStd
+{
+    public static partial class CommonExtensions
+    {
+        /// <summary>
+        ///  return string.Format(value, arg0) where value is the given string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="arg0">An Object to format</param>
+        /// <returns>string.Format(value, arg0)</returns>
+        public static string Format(this string value, object arg0)
+        {
+            return string.Format(value, arg0);
+        }
+
+        /// <summary>
+        ///  return string.Format(value, args) where value is the given string 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="args">An array containing objects to format</param>
+        /// <returns>string.Format(value, args)</returns>
+        public static string Format(this string value, params object[] args)
+        {
+            return string.Format(value, args);
+        }
+    }
+}
