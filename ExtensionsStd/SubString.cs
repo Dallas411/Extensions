8377947792b50f47ef6d142752a35c9a5303bcc7
Marco Dalla Libera <marco@dallalibera.net>
2018-07-03 17:29:57 +0200

    Substring

---
+++ ExtensionsStd/SubString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionsStd
+{
+    public static partial class CommonExtensions
+    {
+        /// <summary>
+        /// Never throw an exception like
+        /// instead it happens with standard String.substring
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="skip">number of char to skip</param>
+        /// <param name="take">number of char to return</param>
+        /// <returns></returns>
+        public static string Substring(this string value, int skip, int take)
+        {
+            return new string(value.Skip(skip).Take(take).ToArray());
+        }
+    }
+}
