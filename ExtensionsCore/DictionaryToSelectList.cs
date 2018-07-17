1bcf8f22c506b1c4033d762b3e9095b3c5f26551
Marco Dalla Libera <marco@dallalibera.net>
2018-07-17 09:56:43 +0200

    DictionaryToSelectList

---
+++ ExtensionsCore/DictionaryToSelectList.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionsCore
+{
+    public static partial class CommonExtensions
+    {
+        public static SelectList ToSelectList(this Dictionary<string, string> keyValue, object selected)
+        {
+            if (selected == null)
+                return new SelectList(keyValue.ToList(), "Key", "Value");
+            else
+                return new SelectList(keyValue.ToList(), "Key", "Value", selected);
+        }
+    }
+}
