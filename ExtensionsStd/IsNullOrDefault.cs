2b696f385b83d1f11a93c8a6f3b1460b17f4f184
Marco Dalla Libera <marco@dallalibera.net>
2018-07-23 14:42:57 +0200

    IsNullOrDefault

---
+++ ExtensionsStd/IsNullOrDefault.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionsStd
+{
+    public static partial class CommonExtensions
+    {
+        /// <summary>
+        /// Is null or default
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="value">Value to evaluate</param>
+        /// <returns>Result</returns>
+        public static bool IsNullOrDefault<T>(this T? value) where T : struct
+        {
+            return default(T).Equals(value.GetValueOrDefault());
+        }
+    }
+}
