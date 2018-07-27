0fd9cf2ebf26a9659fe59988fb5de09c86254779
Marco Dalla Libera <marco@dallalibera.net>
2018-07-27 14:49:31 +0200

    Move the item selected by predicate in first position of current array

---
+++ ExtensionsStd/ArrayMoveFirst.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionsStd
+{
+    public static partial class CommonExtensions
+    {
+        /// <summary>
+        /// Move the item selected by predicate in first position of current array
+        /// arr.MoveToFront(c => c.ID == 1085);
+        /// </summary>
+        public static bool MoveFirst<T>(this T[] arr, Predicate<T> match)
+        {
+            if (arr.Length == 0)
+            {
+                return false;
+            }
+            var id = Array.FindIndex(arr, match);
+            if (id == -1)
+            {
+                return false;
+            }
+            var ret = arr[id];
+            Array.Copy(arr, 0, arr, 1, id);
+            arr[0] = ret;
+            return true;
+        }
+    }
+}
