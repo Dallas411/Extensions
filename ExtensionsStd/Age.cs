9528ab1fb36b32d0f9fb9dfcfbd981002a1dbb82
Marco Dalla Libera <marco@dallalibera.net>
2020-04-28 12:18:56 +0200

    Add Age method

---
+++ ExtensionsStd/Age.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionsStd
+{
+    public static partial class CommonExtensions
+    {
+        /// <summary>
+        /// Return current age based on date of birth
+        /// </summary>
+        /// <param name="dob"></param>
+        /// <returns></returns>
+        public static int Age(this DateTime dateOfBirth)
+        {
+            int age = 0;
+            age = DateTime.Now.Year - dateOfBirth.Year;
+            if (DateTime.Now.DayOfYear < dateOfBirth.DayOfYear)
+                age = age - 1;
+
+            return age;
+        }
+    }
+}
